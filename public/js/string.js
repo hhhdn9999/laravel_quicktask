ff77be9d90de161def4008466d9c29f27f5e0e52
HUYNH HUY HOANG <huynhhuyhoang9999@gmail.com>
2020-08-12 10:18:46 +0700

    auth

---
+++ public/js/string.js
@@ -0,0 +1,19 @@
+// Ajax Logout
+$(document).ready(function () {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $('#logout_btn').click(function () {
+        $.ajax
+        ({
+            type: 'POST',
+            url: ('/logout'),
+            success: function()
+            {
+                window.location.href = '/login';
+            }
+        });
+    });
+});
